c71f6b9e761d3b4384c32a74a98c9b2f4cbced83
Hatori <hatomasa1184@gmail.com>
2021-12-14 00:25:41 +0900

    ignore更新

---
+++ Tank/Tank/source/Component/Debug/DebugPause.cpp
@@ -0,0 +1,35 @@
+﻿//-------------------------
+//	デバッグポーズ
+//-- author --
+//	HatoriMasashi
+//-------------------------
+
+//-- include --
+#include "DebugPause.h"
+#include "MySystem\KeyInput\KeyInput.h"
+#include "Scene\SceneManager.h"
+
+DebugPause::DebugPause()
+{
+	m_IsPausing = false;
+}
+
+void DebugPause::Update()
+{
+	bool prev = m_IsPausing;
+	if (KeyInput::GetKeyPush(VK_F1))
+		m_IsPausing ^= 1;
+
+	if (prev == m_IsPausing) return;
+
+	//-- シーン内のPause状態を変更 --
+	auto manager = SceneManager::GetInstance().GetCurrentScene()->manager;
+	auto list = manager->GetList();
+	for (auto obj : list)
+	{
+		obj->SetPause(m_IsPausing);
+	}
+	//-- カメラは停止しない --
+	auto camera = manager->FindObjectWithTag("Camera");
+	camera->SetPause(false);
+}
---
+++ Tank/Tank/source/Component/Debug/DebugPause.h
@@ -0,0 +1,23 @@
+﻿//-------------------------
+//	デバッグポーズ
+//-- author --
+//	HatoriMasashi
+//-------------------------
+#pragma once
+
+//-- include --
+#include "Component\Component.h"
+
+//-- クラス定義 --
+class DebugPause : public Component
+{
+public:
+	DebugPause();
+	virtual ~DebugPause() = default;
+
+public:
+	void Update() override;
+
+	bool m_IsPausing;
+};
+
